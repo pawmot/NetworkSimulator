4503c37833dc6a5c0561fc374ecc616001575b50
Paweł Motyl <pawmot@gmail.com>
2015-09-18 20:31:40 +0200

    Router sends a RIPv1 request after being enabled

--- src/main/scala-2.11/com/pawmot/networkSimulator/NetworkRouterActor.scala
+++ src/main/scala-2.11/com/pawmot/networkSimulator/NetworkRouterActor.scala
@@ -1,16 +1,35 @@
 package com.pawmot.networkSimulator
 
-import akka.actor.Actor
+import akka.actor.{ActorRef, Actor}
+import com.pawmot.networkSimulator.NetworkLinkActor.Connected
 
 import scala.collection.mutable
 
 class NetworkRouterActor(val Addr: Int) extends Actor with NetworkDevice {
+  import com.pawmot.networkSimulator.NetworkRouterActor._
+
+  var enabled = false
+  var links = List[ActorRef]()
   var routingTable = mutable.Map[Int, RoutingEntry]()
 
   override def receive: Receive = netDev orElse {
     case m@Message(_, addr) =>
       routingTable.get(addr).foreach(re => re.link ! m)
+
+    case Connected =>
+      links ::= sender()
+
+    case Enable =>
+      if(!enabled) {
+        enabled = true
+        for(l <- links) l ! RIPv1Request
+      }
   }
 
-  override def handle(content: String): Unit = ???
+  override def handle(content: String): Unit = {}
+}
+
+object NetworkRouterActor {
+  case object Enable
+  case object RIPv1Request
 }
--- src/test/scala-2.11/com/pawmot/networkSimulator/tests/RIPv1Spec.scala
+++ src/test/scala-2.11/com/pawmot/networkSimulator/tests/RIPv1Spec.scala
@@ -1,5 +1,42 @@
 package com.pawmot.networkSimulator.tests
 
-class RIPv1Spec {
+import akka.actor.ActorSystem
+import akka.testkit.{TestProbe, TestActorRef}
+import com.pawmot.networkSimulator.NetworkLinkActor.Connected
+import com.pawmot.networkSimulator.NetworkRouterActor
+import com.pawmot.networkSimulator.NetworkRouterActor.{RIPv1Request, Enable}
+import com.pawmot.networkSimulator.tests.util.UnitSpec
 
+class RIPv1Spec extends UnitSpec[NetworkRouterActor](ActorSystem("RIPv1Spec")) {
+  override def makeActor(): TestActorRef[NetworkRouterActor] = TestActorRef(new NetworkRouterActor(10))
+
+  "NetworkRouterActor" should {
+    "send a RIPv1RequestMessage after being enabled" in {
+      val a = makeActor()
+
+      val p = TestProbe()
+
+      a.tell(Connected, p.ref)
+
+      a ! Enable
+
+      p.expectMsg(RIPv1Request)
+    }
+
+    "not send a RIPv1RequestMessage if it's already enabled" in {
+      val a = makeActor()
+
+      val p = TestProbe()
+
+      a.tell(Connected, p.ref)
+
+      a ! Enable
+
+      p.receiveN(1)
+
+      a ! Enable
+
+      p.expectNoMsg()
+    }
+  }
 }
